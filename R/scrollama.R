6470a5eb578db45fac474dc86b4424fe761ce12b
Siqi Zhang <iqis.gnahz@gmail.com>
2019-04-15 17:36:05 -0500

    add scrollama init function()

--- R/scrollama.R
+++ R/scrollama.R
@@ -28,3 +28,42 @@ html_scroll = function(...) {
 
 # add Scrollama css class & attrs to rmd sections
 # <div class = "section step", data-step: , data-scrollama-...>
+
+
+
+#' Scrollama Initilization Script
+#'
+#' @param params `list` of elements step, debug, offset
+#'
+#' @return `list`
+#' @export
+#'
+#' @examples
+scrollama_init_setup <- function(params = list(step = ".level1",
+                                               debug = "true",
+                                               offset = "0.5")
+  ){
+  init_script <- paste0(
+    "var scroller = scrollama();
+    function handleStepEnter(response) {
+      console.log(response);
+    	response.element.classList.add('is-active');
+    }
+    function handleStepExit(response) {
+    	response.element.classList.remove('is-active');
+    }
+
+    function init() {
+    	scroller.setup({
+    		step: '", params$step, "',
+    		debug: ", params$debug,"
+    		offset: ", params$offset,"
+    	})
+    		.onStepEnter(handleStepEnter)
+    		.onStepExit(handleStepExit);
+    	window.addEventListener('resize', scroller.resize);
+    }"
+  )
+
+  htmltools::tags$script(init_script)
+}
