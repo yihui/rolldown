2e1b471dfa476daa2c1a28bbd22c6a24088eadb5
Siqi Zhang <iqis.gnahz@gmail.com>
2019-04-15 16:15:08 -0500

    fix extra_dependencies: add package arg

--- R/scrollama.R
+++ R/scrollama.R
@@ -1,4 +1,4 @@
-#' Convert to Scrollama
+#' Convert to Scrollama Document
 #'
 #' @param ...
 #'
@@ -8,22 +8,23 @@
 #' @examples
 html_scroll = function(...) {
  rmarkdown::html_document(...,
-                          extra_dependencies = htmltools::htmlDependencies(
+                          extra_dependencies =
                             list(
                             htmltools::htmlDependency(
                               name = "intersection_observer",
                               version = "0.5.1",
                               src = "resources",
+                              package = "rolldown",
                               script = "intersection-observer.js"),
                             htmltools::htmlDependency(
                              name = "scrollama",
                              version = "2.0.0",
                              src = "resources",
+                             package = "rolldown",
                              script = "scrollama.js")
-                            ))
+                            )
                           )
 }
 
 # add Scrollama css class & attrs to rmd sections
 # <div class = "section step", data-step: , data-scrollama-...>
-#
