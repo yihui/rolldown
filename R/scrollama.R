7f05248c2f8c7387f15a3efffdad9f29303eaebd
Siqi Zhang <iqis.gnahz@gmail.com>
2019-04-16 13:43:43 -0500

    directly get /R/* from master

--- R/scrollama.R
+++ R/scrollama.R
@@ -1,69 +1,35 @@
-#' Convert to Scrollama Document
+#' An R Markdown output format for using Scrollama
 #'
-#' @param ...
-#'
-#' @return
+#' This output format will include the Scrollama JS library in the HTML output
+#' from R Markdown.
+#' @param ... Arguments to be passed to
+#'   \code{bookdown::\link{html_document2}()}.
+#' @importFrom htmltools htmlDependency tags HTML
 #' @export
-#'
-#' @examples
-html_scroll = function(...) {
- rmarkdown::html_document(...,
-                          extra_dependencies =
-                            list(
-                            htmltools::htmlDependency(
-                              name = "intersection_observer",
-                              version = "0.5.1",
-                              src = "resources",
-                              package = "rolldown",
-                              script = "intersection-observer.js"),
-                            htmltools::htmlDependency(
-                             name = "scrollama",
-                             version = "2.0.0",
-                             src = "resources",
-                             package = "rolldown",
-                             script = "scrollama.js")
-                            )
-                          )
+scrollama <- function(...) {
+  html_format(..., .dependencies = list(
+    htmlDependency(
+      'scrollama', '2.0.0', pkg_resource('scrollama'), script = 'scrollama.min.js')
+  ))
 }
 
-# add Scrollama css class & attrs to rmd sections
-# <div class = "section step", data-step: , data-scrollama-...>
-
-
-
-#' Scrollama Initilization Script
-#'
-#' @param params `list` of elements step, debug, offset
-#'
-#' @return `list`
+#' @param options A list of initialization options for Scrollama.
+#' @rdname scrollama
 #' @export
-#'
-#' @examples
-scrollama_init_setup <- function(params = list(step = ".level1",
-                                               debug = "true",
-                                               offset = "0.5")){
-  init_script <- paste0(
-    "var scroller = scrollama();
-    function handleStepEnter(response) {
-      console.log(response);
-    	response.element.classList.add('is-active');
-    }
-    function handleStepExit(response) {
-    	response.element.classList.remove('is-active');
-    }
-
-    function init() {
-    	scroller.setup({
-    		step: '", params$step, "',
-    		debug: ", params$debug,"
-    		offset: ", params$offset,"
-    	})
-    		.onStepEnter(handleStepEnter)
-    		.onStepExit(handleStepExit);
-    	window.addEventListener('resize', scroller.resize);
-    }"
-  )
-
-  htmltools::tags$script(init_script)
+#' @examples rolldown::scrollama_setup(list(step = '.level1', offset = .2))
+scrollama_setup <- function(
+  options = list(step = '.section, img, video, embed', offset = .5)
+) {
+  init = sprintf('(function() {
+  var scroller = scrollama();
+  scroller.setup(%s)
+    .onStepEnter(res => {
+      res.element.classList.add("is-active");
+    })
+    .onStepExit(res => {
+      res.element.classList.remove("is-active");
+    });
+  window.addEventListener("resize", scroller.resize);
+})();', to_json(options))
+  tags$script(HTML(init))
 }
-
