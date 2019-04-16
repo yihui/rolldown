4c33e4197822c7cb93babc45069bb5bb3cfbf265
Siqi Zhang <iqis.gnahz@gmail.com>
2019-04-16 14:03:41 -0500

    add style default ()

---
+++ R/style.R
@@ -0,0 +1,16 @@
+#' Default Rolldown Style
+#' @export
+#'
+rolldown_style_default <- function (...) {
+  style <- "
+  .level1 {
+    min-height: 400px;
+    border: 1px solid;
+    margin-bottom: 4em;
+    padding: 1em 2em 2em;
+  }
+  .is-active {
+    background-color: yellow;
+  }"
+  tags$style(HTML(style))
+}
